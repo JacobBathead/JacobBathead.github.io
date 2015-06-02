ee3302b437dd863a418e2b4fed8b064260939216
Jacob Bathead <batman.jacob@hotmail.co.uk>
2015-06-02 08:58:10 +0100

    0.1.5
    Changed javascript on the carousel

--- css/owl-carousel/main.js
+++ css/owl-carousel/main.js
@@ -1,4 +1,12 @@
 //Calling the plugin
 $(document).ready(function(){
   $(".owl-carousel").owlCarousel();
+ 
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+ 
+      items : 2,
+      itemsDesktop : [690,291],
+ 
+  });
+ 
 });
